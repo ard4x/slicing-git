fccc69ea4806ba1c718a739a868a649a88371661
Korotovsky Taras <regedit.my@gmail.com>
2012-01-11 11:41:04 +0200

    file str changed

---
+++ ARCHIVE/YANGUTU-FB/Top100 - Rater/ie6.js
@@ -0,0 +1,3 @@
+// JavaScript Document
+
+DD_belatedPNG.fix('*img,.pp,.mess1,.mess2,.mess3,.mess4,.mess5,.mess6,.mess7,.mess8,.addFav,.cntr,.cntr SPAN,.p_inf_left,.ya_mbx3,.ya_mbx_mid3,.ya_mbx_bot3,.arrow_left');
--- yanguru-FB/yangutu/js/ie6.js
+++
@@ -1,3 +0,0 @@
-// JavaScript Document
-
-DD_belatedPNG.fix('*img,.pp,.mess1,.mess2,.mess3,.mess4,.mess5,.mess6,.mess7,.mess8,.addFav,.cntr,.cntr SPAN,.ya_mbx3,.ya_mbx_mid3,.ya_mbx_bot3,.arrow_left,.p_inf_left');
