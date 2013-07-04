65be9b6ab5745a3c5962d706e7a0c906ae62499b
ard4x <resp.by@gmail.com>
2013-07-04 14:35:30 +0300

    Video mail

---
+++ SITE/js/jquery.textCounter.js
@@ -0,0 +1,55 @@
+/**
+ * User: ard4x
+ * Date: 02.07.13
+ */
+(function($){
+    jQuery.fn.textCounter = function(options){
+
+        options = $.extend({
+            wrapper : '._jTextCounter',       // Главный блок
+            field : '._jTextCounter_field', // Поле, в котором считаем символы
+            counter : '._jTextCounter_counter', // Индикатор количества символов
+            counter_limit : '._jTextCounter_counter_limit', // индикатор минимального количества символов
+            button : '._jTextCounter_button' // Кнопка, которая подтверждает ввод, перехватываем ее нажатия для проверки условий
+        }, options);
+
+        var make = function(){
+
+            var block = $(this),
+                field = $(options.field, block),
+                counter = $(options.counter, block),
+                counter_limit = $(options.counter_limit, block),
+                button = $(options.button, block),
+                limit = $(block).data('limit'),
+
+                // Обработчик подсчета текста
+                textCount = function(e){
+                    var countNumb = $(field).val().length;
+                    $(counter).text( countNumb );
+                    if (countNumb >= limit ){
+                        $(counter).addClass('ok');
+                        $(block).removeClass('error');
+                    }else{
+                        $(counter).removeClass('ok');
+                    }
+                },
+
+                // Обработчик нажатия кнопки
+                buttonCheck = function (e) {
+                    if ($(field).val().length < limit) {
+                        $(block).addClass('error');
+                        e.preventDefault();
+                        e.stopPropagation();
+                    }
+                };
+
+            $(counter_limit).text(limit);
+            $(field).on('keyup', textCount).trigger('keyup');
+            $(button).on('click', buttonCheck);
+        };
+
+        return $(this).each(make);
+        // в итоге, метод responsiveBlock вернет текущий объект jQuery обратно
+
+    };
+})(jQuery);
