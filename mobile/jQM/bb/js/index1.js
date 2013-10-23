a8d3fb7182987fb1472bf26492550317af5bf850
ard4x <resp.by@gmail.com>
2013-10-23 16:25:01 +0300

    1

--- mobile/jQM/bb/js/index1.js
+++ mobile/jQM/bb/js/index1.js
@@ -1,17 +1,75 @@
 $(function(){
-
-    var Start = Backbone.View.extend({
-        el: $("#start"), // DOM элемент widget'а
-        events: {
-            "click input:button": "check" // Обработчик клика на кнопке "Проверить"
-        },
-        check: function () {
-            if (this.el.find("input:text").val() == "test") // Проверка текста
-                controller.navigate("/success", true); // переход на страницу success
-            else
-                controller.navigate("/error", true); // переход на страницу error
+    var UserNameModel = Backbone.Model.extend({ // Модель пользователя
+        defaults: {
+            "Name": ""
         }
     });
+    var Family = Backbone.Collection.extend({ // Коллекция пользователей
+        model: UserNameModel,
+        checkUser: function (username) { // Проверка пользователя
+            var findResult = this.find(function (user) { return user.get("Name") == username })
+            return findResult != null;
+        }
+    });
+    var AppState = Backbone.Model.extend({
+        defaults: {
+            username: "",
+            state: "start"
+        }
+    });
+
+    var MyFamily = new Family([ // Моя семья
+        { Name: "Саша" },
+        { Name: "Юля" },
+        { Name: "Елизар" }
+    ]);
+
+    var AppState = Backbone.Model.extend({
+        defaults: {
+            username: "",
+            state: "start"
+        }
+    });
+
+    var appState = new AppState();
+
+    var Family = ["Саша", "Юля", "Елизар"]; // Моя семья
+    var Views = { };
+
+    var Block = Backbone.View.extend({
+
+        templates: { // Шаблоны на разное состояние
+            "start": _.template($('#start').html()),
+            "success": _.template($('#success').html()),
+            "error": _.template($('#error').html())
+        },
+
+        initialize: function () { // Подписка на событие модели
+            this.model.bind('change', this.render, this);
+        },
+
+        render: function () {
+
+            var state = this.model.get("state");
+            $(this.el).html(this.templates[state](this.model.toJSON()));
+            return this;
+        },
+
+        check: function () {
+            var username = this.el.find("input:text").val();
+            var find = MyFamily.checkUser(username); // Проверка имени пользователя
+            appState.set({ // Сохранение имени пользователя и состояния
+                "state": find ? "success" : "error",
+                "username": username
+            });
+        }
+
+    });
+
+
+
+
+
     var Controller = Backbone.Router.extend({
         routes: {
             "": "start", // Пустой hash-тэг
@@ -21,26 +79,30 @@ $(function(){
         },
 
         start: function () {
-            $(".block").hide(); // Прячем все блоки
-            $("#start").show(); // Показываем нужный
+            appState.set({ state: "start" });
         },
 
         success: function () {
-            $(".block").hide();
-            $("#success").show();
+            appState.set({ state: "success" });
         },
 
         error: function () {
-            $(".block").hide();
-            $("#error").show();
+            appState.set({ state: "error" });
         }
     });
-
     var controller = new Controller(); // Создаём контроллер
 
+    appState.bind("change:state", function () { // подписка на смену состояния для контроллера
+        var state = this.get("state");
+        if (state == "start")
+            controller.navigate("/", false); // false потому, что нам не надо
+        // вызывать обработчик у Router
+        else
+            controller.navigate("/" + state, false);
+    });
 
-
-    var start = new Start();
+    var block = new Block({ model: appState });
+    appState.trigger("change");
 
     Backbone.history.start();  // Запускаем HTML5 History push
 
